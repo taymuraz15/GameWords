2aab074fa02553c51b11b1030b4ef3eec19e90f4
taymuraz <tamutamutautau@gmail.com>
2026-05-04 23:49:26 +0300

    Добавление класса Word

---
+++ NewGameFindWords/Word.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NewGameFindWords
+{
+    public class Word
+    {
+        public string text { 
+            get; 
+            private set; 
+        }
+        public bool isFound {
+            get; 
+            set; 
+        }
+        public List<(int X, int Y)> busyCells { 
+            get; 
+            private set; 
+        }
+        public Word(string s, List<(int X, int Y)> coordinates)
+        {
+            text = s.ToUpper();
+            busyCells = coordinates;
+            isFound = false;
+        }
+        public bool ContainsCell(int x, int y)
+        {
+            foreach (var cell in busyCells)
+            {
+                if (cell.X == x && cell.Y == y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
